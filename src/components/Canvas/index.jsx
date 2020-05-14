0d5cf2a89832f2ecf197aec2999c66b19979aed5
prathamnemade <prathameshnemade95@gmail.com>
2020-05-14 23:44:18 +0530

    front home page code

--- src/components/Canvas/index.jsx
+++ src/components/Canvas/index.jsx
@@ -52,7 +52,7 @@ const CanvasComponent = () => {
     width: 75,
     height: 75,
   }
-  const strokeColor = "#181818"
+  const strokeColor = "#ffffff"
   const strokeWidth = 1
   const segments = 5
   const radius = 7
--- src/components/HomePage/index.jsx
+++ src/components/HomePage/index.jsx
@@ -2,17 +2,49 @@ import React from "react"
 import styled from "styled-components"
 import Typing from "react-typing-animation"
 
+let CodeWrapper = styled.div`
+color: #ffffff;
+`
 const HomePage = () => {
   return (
     <>
-      <Typing>
-        <div>
-          &#60;html&#62;
+      <CodeWrapper>
+        <Typing>
+          <div>
+            &#60;html&#62;
           <br />
-          <Typing.Delay ms={1000} />
-          then this will be typed.
+            <Typing.Delay ms={1000} />
+            <Typing.Backspace count={7} />
+            &#60;!DOCTYPE html&#62;
+            <br />
+            &#60;html&#62;
+            <br />
+            &#60;head&#62;
+            <br />
+            &#60;title&#62;    Prathamesh's Portfolio
+            <br />
+            &#60;/title&#62;
+            <br />
+            &#60;/head&#62;
+            <br />
+            &#60;body&#62;
+            <br />
+            &#60;h3&#62;
+            <br />
+            Hi, I am Prathamesh
+            <br />
+            Front End Developer
+            <Typing.Backspace count={19} />
+            Full Stack Web Developer | Freelancer
+            <br />
+            &#60;/h3&#62;
+            <br />
+            &#60;/body&#62;
+            <br />
+            &#60;/html&#62;
         </div>
-      </Typing>
+        </Typing>
+      </CodeWrapper>
     </>
   )
 }
