71fa77fe6774824c84464313a185357708a27ba4
prathamnemade <prathameshnemade95@gmail.com>
2020-05-16 01:11:33 +0530

    icons and homepage design

--- src/components/HomePage/index.jsx
+++ src/components/HomePage/index.jsx
@@ -1,114 +1,126 @@
 import React from "react"
 import styled from "styled-components"
 import Typing from "react-typing-animation"
+import "./homepage.scss"
+import CrossIcon from "../../assets/icons/cross"
+import LeftCircleBoldIcon from "../../assets/icons/left-circle-bold"
+import RightCircleBlankIcon from "../../assets/icons/right-circle-blank"
 
-let CodeWrapper = styled.div`
-  .html-cursive {
-    font-family: Sacramento;
-    font-style: italic;
-    font-size: 1.3em;
-    color: var(--prathamesh-green);
-    font-weight: 200;
-    letter-spacing: 3px;
-    margin: 0;
-    &.light-bold {
-      color: var(--prathamesh-purple);
+let ColorWrapper = styled.div`
+  display: flex;
+  width: 100%;
+  height: 100%;
+  .middle-text-box {
+    position: absolute;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: inherit;
+    height: inherit;
+    .hello-text {
       font-family: Montserrant;
-      font-weight: 300;
-      font-style: normal;
-      letter-spacing: 0px;
+      font-weight: 900;
+      font-size: 5em;
+      color: var(--prathamesh-red);
+      margin: 0;
+    }
+    .world-text {
+      font-family: Montserrant;
+      font-weight: 900;
+      font-size: 5em;
+      color: var(--prathamesh-black);
+      margin: 0;
     }
   }
-
-  .title-header {
+  .my-description {
+    position: absolute;
+    bottom: 0;
+    left: 20%;
+    bottom: 5%;
     font-family: Montserrant;
-    font-weight: 900;
-    font-size: 3.5em;
-    color: var(--prathamesh-red);
-    letter-spacing: -1px;
-    margin: 0;
-  }
-  .h3-tag {
-    font-family: Montserrant;
-    font-weight: 600;
-    font-size: 5em;
-    color: var(--prathamesh-yellow);
-    margin: 0;
-    span {
-      color: var(--prathamesh-black);
+    font-size: 1em;
+    color: var(--prathamesh-midnight-green);
+    .my-name {
+      font-weight: 400;
+    }
+    .my-mistake,
+    .my-aim {
       font-weight: 900;
     }
   }
-  .h3-designation {
-    font-family: Montserrant;
-    font-weight: 600;
-    font-size: 2.5em;
-    color: var(--prathamesh-purple);
-    margin: 0;
-    span {
-      color: var(--prathamesh-black);
-      font-weight: 500;
+  .yellow-background {
+    flex: 1;
+    background-color: var(--prathamesh-yellow);
+  }
+  .white-background {
+    flex: 1;
+    background-color: var(--prathamesh-white);
+  }
+  .cross-icon {
+    position: absolute;
+    top: 20px;
+    left: 0;
+    right: 0;
+    margin: auto;
+    width: fit-content;
+    text-align: center;
+  }
+  .left-right-icon {
+    position: absolute;
+    top: 20px;
+    left: 0;
+    right: 0;
+    margin: auto;
+    display: flex;
+    width: fit-content;
+    justify-content: space-between;
+    text-align: center;
+    .left-arrow {
+      padding-right: 10px;
+    }
+    left-arrow {
+      padding-left: 10px;
     }
   }
-
-  .intend-1 {
-    margin-left: 20px;
-  }
-  .intend-2 {
-    margin-left: 40px;
-  }
-  .intend-3 {
-    margin-left: 60px;
-  }
 `
-
 const HomePage = () => {
   return (
     <>
-      <Typing>
-        <CodeWrapper>
-          <Typing.Speed ms={4} />
-          <p className="html-cursive">&#60;html&#62;</p>
-
-          <Typing.Delay ms={1000} />
-          <Typing.Backspace count={7} />
-          <p className="html-cursive light-bold">&#60;!DOCTYPE html&#62;</p>
-
-          <p className="html-cursive">&#60;html&#62;</p>
-
-          <p className="html-cursive intend-1">&#60;head&#62;</p>
-
-          <p className="html-cursive intend-2">&#60;title&#62;</p>
-
-          <p className="title-header intend-3">Portfolio</p>
-
-          <p className="html-cursive intend-2">&#60;/title&#62;</p>
-
-          <p className="html-cursive intend-1">&#60;/head&#62;</p>
-
-          <p className="html-cursive intend-1">&#60;body&#62;</p>
-
-          <p className="html-cursive intend-2">&#60;h3&#62;</p>
-          <Typing.Speed ms={1} />
-          <p className="h3-tag intend-3">
-            {" "}
-            Hi, <span>I am Prathamesh</span>
-          </p>
-
-          <p className="h3-designation intend-3">Front End Developer</p>
-          <Typing.Delay ms={1000} />
-          <Typing.Backspace count={19} />
-          <p className="h3-designation intend-3">
-            Full Stack Web Developer <span>|</span> Freelancer
-          </p>
-          <Typing.Speed ms={4} />
-          <p className="html-cursive intend-2">&#60;/h3&#62;</p>
-
-          <p className="html-cursive intend-1">&#60;/body&#62;</p>
-
-          <p className="html-cursive">&#60;/html&#62;</p>
-        </CodeWrapper>
-      </Typing>
+      <ColorWrapper>
+        <div className="middle-text-box">
+          <Typing>
+            <Typing.Speed ms={100} />
+            <p className="hello-text">HELLO</p>
+            <p className="world-text">WORLD !!</p>
+          </Typing>
+        </div>
+        <div className="my-description">
+          <Typing>
+            <Typing.Delay ms={2000} />
+            <Typing.Speed ms={10} />
+            <p className="my-name">I'm Prathamesh,</p>
+            <p className="my-mistake">FRONT END WEB DEVELOPER</p>
+            <Typing.Backspace count={23} />
+            <p className="my-aim">FULL STACK WEB DEVELOPER | FREELANCER</p>
+          </Typing>
+        </div>
+        <div className="cross-icon">
+          <CrossIcon />
+        </div>
+        <div className="left-right-icon">
+          <span className="left-arrow">
+            <LeftCircleBoldIcon />
+          </span>
+          <span className="right-arrow">
+            <RightCircleBlankIcon />
+          </span>
+        </div>
+        <div className="yellow-background"></div>
+        {/* <span className="menu-window">
+          Menu Window
+        </span> */}
+        <div className="white-background"></div>
+      </ColorWrapper>
     </>
   )
 }
